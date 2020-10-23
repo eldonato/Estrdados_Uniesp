3e7f296429d3a0f4d333c4763006d5b7c658693e
Leonardo Donato <kqjut3@gmail.com>
2020-10-23 18:44:24 -0300

    corrigido exerciciod

---
+++ avaliacao 2/exercicioe.py
@@ -0,0 +1,7 @@
+""" Crie um programa no qual o usuário digitará 5 nomes. 
+Todos os cinco nomes serão salvos no vetor chamado "registros”. Em seguida:
+a) Mostre o que o usuário digitou na tela;
+b) O programa pedirá outro nome, que será adicionado no fim do vetor "registros”;
+c) O programa pedirá outro nome, que será adicionado na 2ª posição do vetor "registros”;
+d) Imprima o resultado na tela. """
+
