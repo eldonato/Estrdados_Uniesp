53663695b0a6e7704bd8397fcde3aa8b02d345e2
Leonardo Donato <kqjut3@gmail.com>
2020-10-23 18:44:36 -0300

    corrigido exerciciod

--- avaliacao 2/exerciciod.py
+++ avaliacao 2/exerciciod.py
@@ -1,22 +1,23 @@
 """ Elabore um programa que cria um array de nomes de países e outro array com nomes de cidades.
 Cada uma das criações devem ser feitas com métodos diferentes. """
 
+
 def main():
     listaPaises = []
     listaCidades = []
 
     qtd = int(input('Quantos países vc quer inserir? '))
-    for i in range(0,qtd):
+    for i in range(0, qtd):
         pais = input('Insira o nome de um país: ')
         listaPaises.append(pais)
-    
+
     qtd = int(input('Quantos cidades vc quer inserir? '))
-    for i in range(0,qtd):
+    for i in range(0, qtd):
         cidade = input('Insira o nome de um país: ')
-        listaCidades.append(cidade)
-    
+        listaCidades.insert(0, cidade)
+
     print(listaPaises)
     print(listaCidades)
-    
-main()
 
+
+main()
