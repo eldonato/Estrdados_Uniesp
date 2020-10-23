8e1c8552b4094c9f9fe679bdcec6a38009f32952
Leonardo Donato <kqjut3@gmail.com>
2020-10-23 18:51:07 -0300

    feito exercicioe

--- avaliacao 2/exercicioe.py
+++ avaliacao 2/exercicioe.py
@@ -5,3 +5,25 @@ b) O programa pedirá outro nome, que será adicionado no fim do vetor "registro
 c) O programa pedirá outro nome, que será adicionado na 2ª posição do vetor "registros”;
 d) Imprima o resultado na tela. """
 
+
+def main():
+    registros = []
+    print('Insira 5 nomes')
+
+    for i in range(0, 5):
+        nome = input(f'Insira o {i+1}º nome: ')
+        registros.append(nome)
+
+    print(registros)
+
+    nome = input('Insira um nome (ele será adicionado no fim do vetor): ')
+    registros.append(nome)
+
+    nome = input(
+        'Insira um nome (ele será adicionado na 2ª posição do vetor): ')
+    registros.insert(1, nome)
+
+    print(registros)
+
+
+main()
